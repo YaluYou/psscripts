52cd59c848b943e2c6ff105069827f293ccbbbe4
YaluYou <yyou311@aucklanduni.ac.nz>
2020-01-13 20:01:37 +1300

    Find all non-sdk-style project file of a path

---
+++ locate-nonsdkprojects.ps1
@@ -0,0 +1,29 @@
+﻿# param([string] $solutionpath)
+
+Set-StrictMode -Version 2
+$ErrorActionPreference = 'Stop'
+
+
+
+Function Get-ProjectFiles([string] $path){
+    if([string]:: IsNullOrEmpty($path)){
+        $path = Get-Location
+    }
+
+    Get-Childitem -Path $path -Include *.csproj -Recurse
+}
+
+$solutionpath = "c:\Projects\WorkFlowMax\project"
+
+$fileList = Get-ProjectFiles -path $solutionpath
+
+
+$result = $fileList | % {
+    $csproj = [xml] (get-content $_)
+    if (Get-Member -InputObject $csproj -Name "xml"){
+        return $_.FullName
+    }
+}
+
+Write-Host $result -Separator "`n"
+Write-Host "Totale number of projects : $($result.length)"
